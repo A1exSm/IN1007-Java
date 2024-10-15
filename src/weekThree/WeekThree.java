7c8ff2012cde8a97ae0c35b7676ca60b519167b2
Alexander Smolowitz <alexander.smolowitz@city.ac.uk>
2024-10-15 11:46:43 +0100

    Week3

--- src/weekThree/WeekThree.java
+++ src/weekThree/WeekThree.java
@@ -1,8 +1,23 @@
 package weekThree;
+import java.util.Scanner;
+
 public class WeekThree {
     public void greet(String[] arguments) {
         //Checks whether any arguments have been provided, and gives an appropriate greeting.
         if (arguments.length > 0) System.out.println("Hello " + arguments[0]);
         else System.out.println("Hello");
+    }
+    public void input(String selection) {
+        String string = "";
+        int integer = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        if (selection.equals("String")) {
+            string = sc.nextLine();
+            System.out.println(string);
+        } else if (selection.equals("Integer")) {
+            integer = sc.nextInt();
+            System.out.println(integer);
+        }
     }
 }
